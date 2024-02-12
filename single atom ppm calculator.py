c476395c8cf2951883652ee48ab3de96da7fb0f5
Muhammed A. Mahmoud <ma.prog.zero@gmail.com>
2024-02-12 22:13:40 +0200

    INITIALLIZATION

---
+++ single atom ppm calculator.py
@@ -0,0 +1,19 @@
+def mass(userPPM):
+    ppm = float(userPPM)
+    vol = float(input('Volume: '))
+    mass = (ppm*vol)/1000
+    return mass
+
+
+def calculatePPM():
+    theRatio = float(
+        input('Enter the ratio of the metal ions in the powder: '))
+    theConcentration = float(input('Enter the desired PPM: '))
+    m = mass(theConcentration)
+    calculatedPPM = m * (1/theRatio)
+    return calculatedPPM
+
+
+if __name__ == "__main__":
+    ppmcalc = calculatePPM()
+    print(ppmcalc)
